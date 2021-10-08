aab9ff5fc7bae82183e7524e615c7e72c1d91806
Andre Paduan <andreppaduan@gmail.com>
2021-10-08 12:51:26 +0200

    logout

--- src/main/java/com/slash/slash/controllers/UserController.java
+++ src/main/java/com/slash/slash/controllers/UserController.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -58,8 +61,14 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    public ResponseEntity<?> closeSession(String userEmail) {
-        return null;
+    @GetMapping("/logout")
+    public ResponseEntity<?> closeSession(HttpServletRequest request, HttpServletResponse response) {
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     public ResponseEntity<?> changePassword(String name, String userPassword, String newPassword) throws UserDoesNotExist, NotAuthorized {
